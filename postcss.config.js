5acc7ea564c328e94d28a1cf361432a4558aa8f6
danielcomelo-osso <danielcomelo@gmail.com>
2025-12-12 22:33:33 -0300

    Refactor PostCSS config to use array for plugins
    Changed plugins configuration from object to array format.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('tailwindcss'), // <--- CORREÇÃO: Usar require('tailwindcss')
+    require('autoprefixer'),
+  ],
 }
