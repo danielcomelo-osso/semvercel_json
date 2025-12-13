1bf6aa97a14a4f4a063ab964531b14a3e0840fc8
danielcomelo-osso <danielcomelo@gmail.com>
2025-12-12 22:42:11 -0300

    Refactor PostCSS plugins configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 export default {
-  plugins: [
-    require('tailwindcss'), // <--- CORREÇÃO: Usar require('tailwindcss')
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
